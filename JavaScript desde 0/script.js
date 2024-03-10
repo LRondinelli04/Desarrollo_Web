6ed2e978266c3c129165b682eb0c220e4456a0a6
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-03-10 17:13:01 -0300

    commit JS

---
+++ JavaScript desde 0/script.js
@@ -0,0 +1,8 @@
+const nom = "Lucas";
+const edad = 18;
+
+if (edad < 20 ){
+    console.log("Hola mundo mi nombre es " + nom + " y tengo " + edad);
+} else {
+    console.log("Soy menor de edad no puedo dar informacion");
+}
