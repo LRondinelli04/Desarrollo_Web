d08f153b6d054beafba64792ceb5b16232a5f3bf
Lucas Rondinelli <127363180+LRondinelli04@users.noreply.github.com>
2024-03-17 16:25:38 -0300

    commit

--- JavaScript desde 0/script.js
+++ JavaScript desde 0/script.js
@@ -1,8 +1,8 @@
-const nom = "Lucas";
-const edad = 18;
+const nom = "Lucas"
+const edad = 18
 
 if (edad < 20 ){
-    console.log("Hola mundo mi nombre es " + nom + " y tengo " + edad);
+    console.log("Hola mundo mi nombre es " + nom + " y tengo " + edad)
 } else {
-    console.log("Soy menor de edad no puedo dar informacion");
+    console.log("Soy menor de edad no puedo dar informacion")
 }
