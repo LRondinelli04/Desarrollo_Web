e5076aa9c2b479988c4603e7425d860f9d5d888b
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-03-18 10:52:58 -0300

    commit

---
+++ JavaScript desde 0/Proyecto Integrador/main.js
@@ -0,0 +1,5 @@
+const costoDeBaseM2 = 1.16;
+let fm = prompt("Ingrese los metros cuadrados de su vivienda");
+
+let resultado = costoDeBaseM2 * fm;
+alert("El resultado del calculo es: $" + resultado);
