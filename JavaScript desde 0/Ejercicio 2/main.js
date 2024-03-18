55d46d64bb0f51202731f7c5e07c80c90b51ed73
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-03-18 12:23:39 -0300

    comentarios Ejercicio2

--- JavaScript desde 0/Ejercicio 2/main.js
+++ JavaScript desde 0/Ejercicio 2/main.js
@@ -1,9 +1,12 @@
+// Constantes
 const ingresoTotal = prompt("Ingrese el monto total");
 
+// Variables 
 let gastosNecesarios = (ingresoTotal * 50) / 100;
 let gastosOpcionales = (ingresoTotal * 30) / 100;
 let ahorroEInversion = (ingresoTotal * 20) / 100;
 
+// Muestra de informacion
 alert(
   "El monto a dedicar de cada categoria es, Gastos Necesarios: " +
     gastosNecesarios +
