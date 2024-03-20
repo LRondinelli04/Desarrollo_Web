b8eed6fb4833fb366edd557027ecfb255b061221
Lucas Rondinelli <127363180+LRondinelli04@users.noreply.github.com>
2024-03-20 19:16:33 -0300

    ejercicio adicional gpt

---
+++ JavaScript desde 0/Ejercicio Adicional (GPT)/main.js
@@ -0,0 +1,11 @@
+// Variables
+let num1;
+let num2;
+
+// Funcion
+function suma(num1, num2) {
+  return num1 + num2;
+}
+
+// Ejecucion de la funcion
+console.log("El resultado de la suma de 3 + 5 es = ", suma(3, 5));
