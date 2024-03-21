120ee6c83681f0fb0560b6276256f656271e855e
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-03-21 10:15:27 -0300

    Proyecto Integrador (Modulo 1)

--- JavaScript desde 0/Proyecto Integrador/main.js
+++ JavaScript desde 0/Proyecto Integrador/main.js
@@ -1,7 +1,12 @@
-const costoDeBaseM2 = 1.16;
-let fm = prompt("Ingrese los metros cuadrados de su vivienda");
+// Constantes fijas de un casa
+const costoDeBase = 1.16;
+const fm = 1.06;
 
-let resultado = costoDeBaseM2 * fm;
+// Variables 
+let m2 = prompt("Ingrese los metros cuadrados de su vivienda");
+
+// Resultado
+let resultado = costoDeBase * m2 * fm;
 alert("El resultado del calculo es: $" + resultado);
 
-// esto es un comentario de prueba 
+// esto es un comentario de prueba
