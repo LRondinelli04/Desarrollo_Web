33c09c7cf50e8dde95363897040e57c9d6240126
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-03-21 20:27:34 -0300

    commit

---
+++ Seminario de Lenguajes - JavaScript/Practica 1/Ejercicio 6/main.js
@@ -0,0 +1,17 @@
+/* 1. Cree dos variables dia1 con la fecha actual y dia2 con el valor 1575978300000 (en timestamp).
+2. Realice una función imprimirFecha que formatee y devuelva una fecha en el formato dd/mm/aaaa hh:mm:ss.
+3. Llame a la función imprimirFecha e imprima en consola el resultado.
+4. Realice una función que reciba dos fechas, asigne el año de dia1 a dia2, y viceversa e imprima en consola
+ambas fechas.
+5. Realice una función que reste dos fechas y retorne una nueva fecha con la diferencia e imprima en consola
+ambas fechas.
+6. Abra el archivo en el navegador y use las herramientas de desarrollo para ver el resultado.
+ */
+
+// Variables
+let dia1 = 21032024
+let dia2 = 1575978300000
+
+function imprimirFecha(dia1, dia2) {
+    
+}
