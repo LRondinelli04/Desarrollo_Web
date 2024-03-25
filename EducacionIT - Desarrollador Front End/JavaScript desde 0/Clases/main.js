7a19477d7f0fed6b045b6befbd948efacb38a635
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-03-25 13:04:21 -0300

    Clase JS desde 0

--- EducacionIT - Desarrollador Front End/JavaScript desde 0/Clases/main.js
+++ EducacionIT - Desarrollador Front End/JavaScript desde 0/Clases/main.js
@@ -2,7 +2,7 @@
    - Almacenamos ese dato en memoria 
 */
 
-const nombre = prompt("Ingrese su nombre de usuario");
+const nombre = "Lucas Rondinelli";
 // Si se cargo un nombre y no es un espacio vacio
 if (nombre !== null && nombre !== "") {
   bienvenido.innerText = "Bienvenido " + nombre + "!";
@@ -17,3 +17,54 @@ const ingles = "Additional Information on Live Classes";
 const frances = "Informations supplémentaires sur les cours en direct";
 // Asignamos contenido dentro del parrafo con el id informacion
 titulo.innerText = espanol;
+
+const mensaje = document.querySelector("#mensaje");
+
+let text = "ArGenTINA";
+if (text.toLowerCase() === "argentina") {
+  /* mensaje.innerText = "Se escribio la palabra Argentina"; */
+  mensaje.innerHTML =
+    '<img src = "/EducacionIT - Desarrollador Front End/img/deporte1.png">';
+} else {
+  mensaje.innerText = "No se escribio la palabra Argenitna";
+}
+
+// Creamos objeto fecha
+
+const texto = document.querySelector("#texto");
+
+const fecha = new Date();
+console.log(fecha);
+// Esto te permite obtener datos especificos de 'fecha'
+console.log(fecha.getFullYear());
+
+// Obtenemos el numero del dia del mes
+let diaMes = fecha.getDate();
+console.log("dia de mes: ", diaMes);
+if (diaMes <= 9) {
+  diaMes = "0" + diaMes;
+}
+
+// Obtenemos el numero del mes actual
+let mes = fecha.getMonth() + 1;
+console.log("mes actual: ", mes);
+if (mes <= 9) {
+  mes = "0" + mes;
+}
+// Obtenemos el numero del año actual
+let anio = fecha.getFullYear();
+console.log("año actual: ", anio);
+
+/* switch (mes) {
+  case 1:
+    mes = "enero";
+    break;
+  case 3:
+    mes = "marzo";
+    break;
+  default:
+    mes = "mes no detectado";
+    break;
+} */
+
+texto.innerText = diaMes + "/" + mes + "/" + anio;
