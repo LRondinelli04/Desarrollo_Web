2553cbb8104a0a0cfffd434eef62853ac4a684cd
lucasrondinelli04 <lucasrondinelli04@gmail.com>
2024-04-11 13:35:51 -0300

    commit

--- EducacionIT - Desarrollador Front End/JavaScript desde 0/Proyecto Integrador/main.js
+++ EducacionIT - Desarrollador Front End/JavaScript desde 0/Proyecto Integrador/main.js
@@ -1,43 +1,39 @@
-// Constantes fijas de un casa
-const costoDeBase = 1.16;
+let fmPropiedad;
+let fmUbicacion;
+const costoM2 = 1.16;
 
-// Variables
-let fm;
+let metros2 = prompt("Ingresa los metros cuadrados de la vivienda: ");
+
 let tipoVivienda = prompt(
-  "Selecciona la vivienda a cotizar: \n" +
-    "('Casa', 'P.H.', 'Dto. Edificio', 'Barrio Privado', 'Oficina', 'Local comercial', 'Depósito logística')"
+  "Selecciona la vivienda a cotizar: \n" + "('Casa', 'P.H.', 'Dto. Edificio')"
 );
-let m2 = prompt("Ingrese los metros cuadrados de su vivienda");
 
-// Resultado
-switch (tipoVivienda) {
-  case "Casa":
-    fm = 1.009;
-    break;
-  case "P.H":
-    fm = 1.005;
-    break;
-  case "Dto. Edificio":
-    fm = 1.002;
-    break;
-  case "Barrio Privado":
-    fm = 1.019;
-    break;
-  case "Oficina":
-    fm = 1.039;
-    break;
-  case "Local comercial":
-    fm = 1.041;
-    break;
-  case "Depósito logística":
-    fm = 1.092;
-    break;
+if (tipoVivienda != "") {
+  for (propiedad of datosPropiedad) {
+    if (propiedad.tipo == tipoVivienda) {
+      fmPropiedad = propiedad;
+      break;
+    }
+  }
 }
 
-// Validamos si el valor de fm es superior a 1.000 y si los metros cuadrado ingresados en un prompt se pueden convertir a un valor entero
-if (fm > 1.0 && Number.parseInt(m2)) {
-  let calculo = costoDeBase * m2 * fm;
-  console.log("El resultado del calculo es: $" + calculo);
+let tipoUbicacion = prompt(
+  "Selecciona la ubicación de la vivienda: \n" +
+    "('CABA', 'Tandil', 'Costa Atlántica')"
+);
+
+if (tipoUbicacion != "") {
+  for (ubicacion of datosUbicacion) {
+    if (ubicacion.tipo == tipoUbicacion) {
+      fmUbicacion = ubicacion;
+      break;
+    }
+  }
+}
+
+if (fmPropiedad && fmUbicacion && parseInt(metros2)) {
+  let resultado = fmPropiedad.factor * fmUbicacion.factor * metros2 * costoM2;
+  console.log("Resultado de la Póliza: $ " + resultado);
 } else {
-  console.warn("Hubo un error en los datos ingresados");
+  console.warn("Hubo un error en los datos ingresados.");
 }
