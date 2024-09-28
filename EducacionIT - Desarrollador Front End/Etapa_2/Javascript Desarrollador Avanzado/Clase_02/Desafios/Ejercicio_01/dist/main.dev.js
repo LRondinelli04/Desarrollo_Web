cfed60690e6567ecaf44ca6bf3e126c5792607a6
LRondinelli04 <lrondinelli2004@gmail.com>
2024-09-28 09:23:08 -0300

    ejercicio 1 modulo 2

---
+++ EducacionIT - Desarrollador Front End/Etapa_2/Javascript Desarrollador Avanzado/Clase_02/Desafios/Ejercicio_01/dist/main.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Consigna
+
+/*
+    Diseñar una función llamada CampoRequerido.
+    Esta función debe recibir un elemento del DOM y
+    retornar el mismo elemento. Solo se debe agregar
+    un listener al ‘blur’ para que, cuando éste se
+    dispare, modifique la propiedad isValid de ese
+    elemento.
+*/
+function CampoRequerido(elemento) {
+  // Agregar el listener al evento blur
+  elemento.addEventListener("blur", function () {
+    // Modificar la propiedad isValid del elemento
+    elemento.isValid = elemento.value.length !== 0; // si el elemento no esta vacio retorna true, sino false
+  }); // Retornar el mismo elemento
+
+  return elemento;
+}
