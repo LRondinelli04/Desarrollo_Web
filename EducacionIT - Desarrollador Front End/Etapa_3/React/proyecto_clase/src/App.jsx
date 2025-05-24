be7e173b3c038093831b1b02f49bccf7ab26effa
LRondinelli04 <lrondinelli2004@gmail.com>
2025-05-24 11:35:48 -0300

    creacion de proyecto integrador y proyecto clase

---
+++ EducacionIT - Desarrollador Front End/Etapa_3/React/proyecto_clase/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Educacion IT</h1>
+      <h3>Curso de React JS</h3>
+    </>
+  );
+}
+
+export default App;
